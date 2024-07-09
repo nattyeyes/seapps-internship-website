7230c625654a4d5572a287fcd479fe6ddd0c620f
Nathan Amata <80802672+nattyeyes@users.noreply.github.com>
2024-07-09 13:28:39 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+
+const app = express();
+
+const db = require("./app/models");
+const dbConfig = require('./app/config/db.config');
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connected to MongoDB");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+function initial() {
+    /*Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'user' to roles collection");
+            });
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'admin' to roles collection");
+            });  
+        }
+    }); */
+    
+    try {
+        const count = Role.estimatedDocumentCount();
+        if (count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'user' to roles collection");
+            });
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    } catch (error) {
+        console.error('Error estimating document count:', error);
+    }
+}
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true}));
+
+app.use(
+    cookieSession({
+        name: "SEAPP-session",
+        keys: ["COOKIE_SECRET"], //should use as secret environment variable
+        httpOnly: true
+    })
+);
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
